fb4371036a739398a7901269eec4b7758dcfcf7c
JianShan-1214 <l059069045@gmail.com>
2023-04-22 17:10:05 +0800

    .

--- src/main.js
+++ src/main.js
@@ -4,30 +4,7 @@ import App from "./App.vue";
 import router from "./router/main.js";
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
-
-// router.beforeEach((to, from, next) => {
-//   const baseURL = "http://localhost:8000";
-//   next()
-//   if (to.meta.requiresAuth) {
-//     const api = `${baseURL}/check`;
-//     fetch(api, {
-//       method: "GET",
-//       headers: {
-//         Authorization: `bearer ${localStorage.getItem("token")}`,
-//       },
-//     }).then((response) => {
-//       if (response.ok) {
-//         next();
-//       } else {
-//         next({
-//           path: "/login",
-//         });
-//       }
-//     });
-//   } else {
-//     next();
-//   }
-// });
+import './style.css'
 
 
 const app = createApp(App)
